59d0b7796446dabf73dc2dedca413b1ab558f9ee
Juraj Paluba <juraj.palubajp@gmail.com>
2026-03-01 10:05:19 +0100

    refactor(config): disable HTTPS and listen on localhost

--- config/runtime.exs
+++ config/runtime.exs
@@ -52,18 +52,15 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "example.com"
-
   config :hivex, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
   config :hivex, HivexWeb.Endpoint,
-    url: [host: host, port: 443, scheme: "https"],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
       # See the documentation on https://hexdocs.pm/bandit/Bandit.html#t:options/0
       # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0}
+      ip: {0, 0, 0, 0, 0, 0, 0, 1}
     ],
     secret_key_base: secret_key_base
 
